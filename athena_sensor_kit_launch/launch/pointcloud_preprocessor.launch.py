d9ae7060c146f02417b12a62fb1aea2af5659feb
Arslan-Z <arslan.z.2019@gmail.com>
2024-04-23 09:36:51 +0800

    change lidar  topic name

--- athena_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ athena_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -37,8 +37,8 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             {
                 "input_topics": [
-                    "/rs_helios_top/points_raw",
-                    "/rs_m1_right/points_raw"
+                    "rslidar_sdk/rs_helios_top/points_raw",
+                    "rslidar_sdk/rs_m1_right/points_raw"
                     # "/sensing/lidar/top/pointcloud_before_sync",
                     # "/sensing/lidar/left/pointcloud_before_sync",
                     # "/sensing/lidar/right/pointcloud_before_sync",
