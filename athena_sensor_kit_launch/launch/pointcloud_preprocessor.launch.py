22c63c10ae955f4d486cdcd597ae97353759c432
Arslan-Z <arslan.z.2019@gmail.com>
2024-04-27 18:18:18 +0800

    start orient branch

--- athena_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ athena_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -1,18 +1,3 @@
-# Copyright 2020 Tier IV, Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.actions import OpaqueFunction
@@ -20,6 +5,7 @@ from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 
@@ -37,30 +23,41 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             {
                 "input_topics": [
-                    # "rslidar_sdk/rs_helios_top/pointcloud",
-                    # "rslidar_sdk/rs_m1_right/pointcloud"
                     "rslidar_sdk/rs_helios_top/points_raw",
-                    "rslidar_sdk/rs_m1_right/points_raw"
-                    # "/sensing/lidar/top/pointcloud_before_sync",
-                    # "/sensing/lidar/left/pointcloud_before_sync",
-                    # "/sensing/lidar/right/pointcloud_before_sync",
+                    "rslidar_sdk/rs_m1_right/points_raw",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
                 "input_twist_topic_type": "twist",
-                "publish_synchronized_pointcloud": True,
             }
         ],
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
+    # set container to run all required components in the same process
+    container = ComposableNodeContainer(
+        name=LaunchConfiguration("container_name"),
+        namespace="",
+        package="rclcpp_components",
+        executable=LaunchConfiguration("container_executable"),
+        composable_node_descriptions=[],
+        condition=UnlessCondition(LaunchConfiguration("use_pointcloud_container")),
+        output="screen",
+    )
+
+    target_container = (
+        container
+        if UnlessCondition(LaunchConfiguration("use_pointcloud_container")).evaluate(context)
+        else LaunchConfiguration("container_name")
+    )
+
     # load concat or passthrough filter
     concat_loader = LoadComposableNodes(
         composable_node_descriptions=[concat_component],
-        target_container=LaunchConfiguration("pointcloud_container_name"),
+        target_container=target_container,
         condition=IfCondition(LaunchConfiguration("use_concat_filter")),
     )
 
-    return [concat_loader]
+    return [container, concat_loader]
 
 
 def generate_launch_description():
@@ -72,7 +69,8 @@ def generate_launch_description():
     add_launch_arg("base_frame", "base_link")
     add_launch_arg("use_multithread", "False")
     add_launch_arg("use_intra_process", "False")
-    add_launch_arg("pointcloud_container_name", "pointcloud_container")
+    add_launch_arg("use_pointcloud_container", "False")
+    add_launch_arg("container_name", "pointcloud_preprocessor_container")
 
     set_container_executable = SetLaunchConfiguration(
         "container_executable",
