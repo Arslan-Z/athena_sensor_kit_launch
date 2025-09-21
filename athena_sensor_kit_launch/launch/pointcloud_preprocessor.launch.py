2f776f8b6181855ecabdf3bcd00976a651fbd5d6
Arslan-Z <arslan.z.2019@gmail.com>
2025-09-21 16:31:04 +0800

    feat: fuse helios32 and m1

--- athena_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ athena_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -38,10 +38,10 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             {
                 "input_topics": [
-                    # "rslidar_sdk/rs_helios_top/points_raw",
-                    # "rslidar_sdk/rs_m1_right/points_raw"
-                    "/sensing/lidar/top/pointcloud_before_sync",
-                    "/sensing/lidar/top/pointcloud_before_sync",
+                    "/sensing/lidar/rslidar_sdk/rs_helios_top/points_raw",
+                    "/sensing/lidar/rslidar_sdk/rs_m1_right/points_raw"
+                    # "/sensing/lidar/top/pointcloud_before_sync",
+                    # "/sensing/lidar/top/pointcloud_before_sync",
                     # "/sensing/lidar/top/pointcloud_raw",
                     # "/sensing/lidar/left/pointcloud_before_sync",
                     # "/sensing/lidar/right/pointcloud_before_sync",
